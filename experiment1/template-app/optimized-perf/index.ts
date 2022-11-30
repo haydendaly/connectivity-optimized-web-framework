2cd39c5660f775dae70d9c72c068f0dea0406173
haydendaly <hcd36@cornell.edu>
2022-11-30 13:08:46 -0500

    add better comments

--- experiment1/template-app/optimized-perf/index.ts
+++ experiment1/template-app/optimized-perf/index.ts
@@ -1,8 +1,10 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
+// this defines the types and interface for the method
 export function determineNetworkQuality(request: NextRequest): NextResponse {
   const requestHeaders = new Headers(request.headers);
+  // this is where our classification algorithm will go
   requestHeaders.set("network-quality", "low");
 
   const response = NextResponse.next({
